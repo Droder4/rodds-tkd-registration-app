d999f8d65cdc8d177cbcad6145498d5f833f4569
Droder4 <roddstkd@gmail.com>
2026-03-11 17:41:33 -0300

    Update Registration.kt

--- app/src/main/java/com/roddstkd/registrationapp/Registration.kt
+++ app/src/main/java/com/roddstkd/registrationapp/Registration.kt
@@ -1,1 +1,25 @@
+package com.roddstkd.registrationapp
 
+data class Registration(
+    val timestamp: String? = "",
+    val studentType: String? = "",
+    val location: String? = "",
+    val email: String? = "",
+    val parentName: String? = "",
+    val contactNumber: String? = "",
+    val studentName: String? = "",
+    val studentAge: String? = "",
+    val birthdate: String? = "",
+    val grade: String? = "",
+    val assignedClass: String? = "",
+    val membershipNoticeAgreed: String? = "",
+    val lateFeeNoticeAgreed: String? = "",
+    val emailStatus: String? = "",
+    val emailSentDate: String? = "",
+    val notes: String? = ""
+)
+
+data class SendEmailResponse(
+    val success: Boolean,
+    val message: String
+)
