8aabb90f02641d9b3f26354c48d3cc0c46e084b5
Droder4 <roddstkd@gmail.com>
2026-03-11 17:41:59 -0300

    Update ApiService.kt

--- app/src/main/java/com/roddstkd/registrationapp/ApiService.kt
+++ app/src/main/java/com/roddstkd/registrationapp/ApiService.kt
@@ -1,1 +1,29 @@
+package com.roddstkd.registrationapp
 
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+    @GET("exec")
+    fun getRegistrations(
+        @Query("action") action: String = "getRegistrations"
+    ): Call<List<Registration>>
+
+    @GET("exec")
+    fun getByClub(
+        @Query("action") action: String = "getByClub",
+        @Query("club") club: String
+    ): Call<List<Registration>>
+
+    @GET("exec")
+    fun getByClass(
+        @Query("action") action: String = "getByClass",
+        @Query("className") className: String
+    ): Call<List<Registration>>
+
+    @GET("exec")
+    fun sendPendingEmails(
+        @Query("action") action: String = "sendPendingEmails"
+    ): Call<SendEmailResponse>
+}
