8bd94b7fa992d22dff87a003392b284440db131c
Droder4 <roddstkd@gmail.com>
2026-03-12 08:52:09 -0300

    Update ApiService.kt

--- app/src/main/java/com/roddstkd/registrationapp/ApiService.kt
+++ app/src/main/java/com/roddstkd/registrationapp/ApiService.kt
@@ -1,7 +1,10 @@
 package com.roddstkd.registrationapp
 
+import okhttp3.RequestBody
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface ApiService {
@@ -26,15 +29,60 @@ interface ApiService {
     @GET("exec")
     fun sendPendingEmails(
         @Query("action") action: String = "sendPendingEmails"
-    ): Call<SendEmailResponse>
+    ): Call<ActionResponse>
 
     @GET("exec")
     fun sendRegistrationCompleteEmails(
         @Query("action") action: String = "sendRegistrationCompleteEmails"
-    ): Call<SendEmailResponse>
+    ): Call<ActionResponse>
 
     @GET("exec")
     fun sendIncompleteRegistrationReminders(
         @Query("action") action: String = "sendIncompleteRegistrationReminders"
-    ): Call<SendEmailResponse>
+    ): Call<ActionResponse>
+
+    @GET("exec")
+    fun getAttendance(
+        @Query("action") action: String = "getAttendance",
+        @Query("location") location: String = "",
+        @Query("className") className: String = "",
+        @Query("date") date: String = ""
+    ): Call<List<AttendanceItem>>
+
+    @GET("exec")
+    fun getBeltTesting(
+        @Query("action") action: String = "getBeltTesting"
+    ): Call<List<BeltTestItem>>
+
+    @GET("exec")
+    fun markWelcomeEmailSent(
+        @Query("action") action: String = "markWelcomeEmailSent",
+        @Query("email") email: String,
+        @Query("studentName") studentName: String
+    ): Call<ActionResponse>
+
+    @POST("exec")
+    fun saveAttendance(
+        @Body body: RequestBody
+    ): Call<ActionResponse>
+
+    @POST("exec")
+    fun saveBeltTesting(
+        @Body body: RequestBody
+    ): Call<ActionResponse>
+
+    @POST("exec")
+    fun updateStudentNotes(
+        @Body body: RequestBody
+    ): Call<ActionResponse>
+
+    @POST("exec")
+    fun sendBeltTestInvitations(
+        @Body body: RequestBody
+    ): Call<ActionResponse>
+
+    @POST("exec")
+    fun sendClassReminders(
+        @Body body: RequestBody
+    ): Call<ActionResponse>
 }
